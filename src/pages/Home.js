b96b3d848de0039524074e52a4bc087720ccabab
mohd noman <mohdnoman2751@gmail.com>
2023-12-20 04:48:37 +0530

    Add home page and basic structure
    - Create Home component with initial content
    - Set up routing for the home page
    - Define main layout components (header, footer)
    - Configure basic styling with CS

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Box } from '@mui/material'
+
+import HeroBanner from '../components/HeroBanner'
+import Exercises from '../components/Exercises'
+import SearchExercises from '../components/SearchExercises'
+
+
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <Box>
+      <HeroBanner />
+      <SearchExercises />
+      <Exercises />
+    </Box>
   )
 }
 
