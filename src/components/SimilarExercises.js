6de0c43ad0222bc9519a0cb807424a0965b03acf
mohd noman <mohdnoman2751@gmail.com>
2023-12-27 04:15:18 +0530

    added exercise detail page and fetched data from youtube Api.

---
+++ src/components/SimilarExercises.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Box, Stack, Typography } from '@mui/material';
+
+const SimilarExercises = ({ targetMuscleExercises, equipmentExercises }) => {
+  return (
+    <div>SimilarExercises</div>
+  )
+}
+
+export default SimilarExercises
