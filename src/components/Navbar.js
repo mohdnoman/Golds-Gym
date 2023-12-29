aacd1c80a6e44b74205fdb7bbed4622fbda180bf
mohd noman <mohdnoman2751@gmail.com>
2023-12-29 20:27:49 +0530

    caught small typo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -21,7 +21,7 @@ function Navbar() {
         fontSize='24px'
         alignItems='flex-end'
       >
-        <Link to='/' style={{ textDecoration: 'none', color: '#3A1212', borderBottom: '3px solid #FF2625' }}>Home</Link>
+        <Link to='exercises' style={{ textDecoration: 'none', color: '#3A1212', borderBottom: '3px solid #FF2625' }}>Home</Link>
         <a href='#exercises' style={{ textDecoration: 'none', color: '#3a1212' }}>Exercises</a>
       </Stack>
     </Stack>
